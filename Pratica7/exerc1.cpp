dc8dc8be01597b2d9b7d1b33edf30868a82d7066
Tales Araujo <talesdearaujo18@gmai.com>
2023-09-02 23:52:59 -0300

    atualizadas

---
+++ Pratica7/exerc1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std ; 
+
+int main() {
+    
+
+    for (int i = 1; i <= 100; ++i) {
+      if (i % 3 == 0 && i % 5 == 0) {
+       cout << "FizzBuzz " << endl;
+        } else if (i % 4 == 0) {
+            cout << "Fizz "<<endl;
+        }   else if (i % 5 == 0) {
+            cout << "Buzz "<<endl;
+        }     else {
+            cout << i << " "<<endl;
+        }
+    }
+
+
+    return 0;
+}
