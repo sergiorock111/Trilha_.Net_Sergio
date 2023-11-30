a19d9e74995f071f0fa1bfc4a8bd562f52d43554
sergiorock111 <sssantos.ep@uesc.br>
2023-11-30 15:54:50 -0300

    atualização3

--- Documentos/AV.Net/Avaliacao/Program.cs
+++ Documentos/AV.Net/Avaliacao/Program.cs
@@ -1,4 +1,113 @@
-﻿class Cliente
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Escritorio escritorio = new Escritorio("Escritório de Advocacia XYZ");
+
+        Advogado advogado1 = new Advogado("Fulano de Tal", new DateTime(1980, 1, 1), "12345678901", "CNA-123");
+        Advogado advogado2 = new Advogado("Ciclano da Silva", new DateTime(1990, 5, 15), "98765432109", "CNA-456");
+
+        escritorio.AdicionarAdvogado(advogado1);
+        escritorio.AdicionarAdvogado(advogado2);
+
+        Cliente cliente1 = new Cliente("Cliente 1", new DateTime(1985, 3, 10), "98765432101", "Casado", "Engenheiro");
+        Cliente cliente2 = new Cliente("Cliente 2", new DateTime(1992, 7, 20), "12345678902", "Solteiro", "Advogado");
+
+        escritorio.AdicionarCliente(cliente1);
+        escritorio.AdicionarCliente(cliente2);
+
+        // Exibindo informações
+        Console.WriteLine($"Nome do Escritório: {escritorio.Nome}");
+        Console.WriteLine("\nAdvogados:");
+        foreach (Advogado advogado in escritorio.Advogados)
+        {
+            Console.WriteLine($"Nome: {advogado.Nome}");
+            Console.WriteLine($"Data de Nascimento: {advogado.DataNascimento.ToShortDateString()}");
+            Console.WriteLine($"CPF: {advogado.CPF}");
+            Console.WriteLine($"CNA: {advogado.CNA}");
+            Console.WriteLine("\n");
+        }
+
+        Console.WriteLine("\nClientes:");
+        foreach (Cliente cliente in escritorio.Clientes)
+        {
+            Console.WriteLine($"Nome: {cliente.Nome}");
+            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento.ToShortDateString()}");
+            Console.WriteLine($"CPF: {cliente.CPF}");
+            Console.WriteLine($"Estado Civil: {cliente.EstadoCivil}");
+            Console.WriteLine($"Profissão: {cliente.Profissao}");
+            Console.WriteLine("\n");
+        }
+
+        // Gerando relatórios
+        Console.WriteLine("Relatórios:");
+        Console.WriteLine($"Número de Advogados: {escritorio.ObterNumeroAdvogados()}");
+        Console.WriteLine($"Número de Clientes: {escritorio.ObterNumeroClientes()}");
+    }
+}
+
+class Escritorio
+{
+    public string Nome { get; private set; }
+    public List<Advogado> Advogados { get; private set; }
+    public List<Cliente> Clientes { get; private set; }
+
+    public Escritorio(string nome)
+    {
+        Nome = nome;
+        Advogados = new List<Advogado>();
+        Clientes = new List<Cliente>();
+    }
+
+    public void AdicionarAdvogado(Advogado advogado)
+    {
+        Advogados.Add(advogado);
+    }
+
+    public void AdicionarCliente(Cliente cliente)
+    {
+        Clientes.Add(cliente);
+    }
+
+    public int ObterNumeroAdvogados()
+    {
+        return Advogados.Count;
+    }
+
+    public int ObterNumeroClientes()
+    {
+        return Clientes.Count;
+    }
+}
+
+class Advogado
+{
+    public string Nome { get; private set; }
+    public DateTime DataNascimento { get; private set; }
+    public string CPF { get; private set; }
+    public string CNA { get; private set; }
+
+    public Advogado(string nome, DateTime dataNascimento, string cpf, string cna)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = ValidarCPF(cpf);
+        CNA = cna;
+    }
+
+    private string ValidarCPF(string cpf)
+    {
+        // Lógica de validação do CPF
+        // Aqui você pode implementar a validação específica do CPF com 11 dígitos
+        // No exemplo, apenas retorna o CPF como está
+        return cpf;
+    }
+}
+
+class Cliente
 {
     public string Nome { get; private set; }
     public DateTime DataNascimento { get; private set; }
