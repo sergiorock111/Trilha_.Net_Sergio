24e876636c6145446d774ee227e4e28b83398872
sergiorock111 <sssantos.ep@uesc.br>
2023-11-30 14:08:16 -0300

    completando a main

---
+++ atividadesala/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Criar uma lista de tuplas com nomes e alturas em centímetros
+        List<(string, int)> pessoas = new List<(string, int)>
+        {
+            ("João", 175),
+            ("Maria", 160),
+            ("Carlos", 180),
+            // Adicione mais tuplas conforme necessário
+        };
+
+        // Usar LINQ e expressão lambda para calcular a altura média
+        double alturaMedia = pessoas.Average(pessoa => pessoa.Item2);
+
+        // Exibir a altura média
+        Console.WriteLine($"Altura média das pessoas: {alturaMedia} centímetros");
+    }
+}
