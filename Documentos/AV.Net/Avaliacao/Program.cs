9e948c1a64c7255b83bc768e5514004e498fcc05
sergiorock111 <sssantos.ep@uesc.br>
2023-11-30 15:48:03 -0300

    atualização3

--- Documentos/AV.Net/Avaliacao/Program.cs
+++ Documentos/AV.Net/Avaliacao/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Cliente
+{
+    public string Nome { get; private set; }
+    public DateTime DataNascimento { get; private set; }
+    public string CPF { get; private set; }
+    public string EstadoCivil { get; private set; }
+    public string Profissao { get; private set; }
+
+    public Cliente(string nome, DateTime dataNascimento, string cpf, string estadoCivil, string profissao)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = ValidarCPF(cpf);
+        EstadoCivil = estadoCivil;
+        Profissao = profissao;
+    }
+
+    private string ValidarCPF(string cpf)
+    {
+      
+        return cpf;
+    }
+}
